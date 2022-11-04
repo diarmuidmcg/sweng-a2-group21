fc56ac58ecdbc5c496842c88d9fd4cab6b55d601
Wiktoria Fabijaniak <fabijanw@tcd.ie>
2022-11-04 18:57:16 +0000

    added all buttons
    added all buttons (no handling imlemented)

--- calculator/frontend/src/App.js
+++ calculator/frontend/src/App.js
@@ -5,18 +5,38 @@ import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
 
+const btnValues = [
+  ["(", ")", "x^y", "C"],
+  ["log", "ln", "e^x", "/"],
+  [7, 8, 9, "x"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
 const App = () => {
+  let [calc, setCalc] = useState({
+    operator: "",
+    num: 0,
+    res: 0,
+  });
+
   return (
     <Wrapper>
-      <Screen value="0" />
+      <Screen value={calc.num ? calc.num : calc.res} />
       <ButtonBox>
-        <Button
-          className=""
-          value="0"
-          onClick={() => {
-            console.log("Button clicked!");
-          }}
-        />
+        {btnValues.flat().map((btn, i) => {
+          return (
+            <Button
+              key={i}
+              className={btn === "=" ? "equals" : btn === "C" ||
+               btn === "/" || btn === "x" || btn === "-" || btn === "+" ? "signs" : ""}
+              value={btn}
+              /*onClick={
+              }*/
+            />
+          );
+        })}
       </ButtonBox>
     </Wrapper>
   );
