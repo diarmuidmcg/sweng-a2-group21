01e237bed73da880ca04fe85eec8faa1bb1f829f
Wiktoria Fabijaniak <fabijanw@tcd.ie>
2022-11-04 18:37:49 +0000

    Added styling and basic implementation
    styled components and rendering

--- calculator/frontend/src/App.js
+++ calculator/frontend/src/App.js
@@ -5,4 +5,21 @@ import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
 
+const App = () => {
+  return (
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        <Button
+          className=""
+          value="0"
+          onClick={() => {
+            console.log("Button clicked!");
+          }}
+        />
+      </ButtonBox>
+    </Wrapper>
+  );
+};
+
 export default App;
